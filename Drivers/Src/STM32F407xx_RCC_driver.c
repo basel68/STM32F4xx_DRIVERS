5ae89ff843a941bfc7ab6561609d9c8828a20529
basel68 <baselfarouk09@gmail.com>
2024-06-25 20:01:18 +0300

    Update STM32F407xx_RCC_driver.c

--- Drivers/Src/STM32F407xx_RCC_driver.c
+++ Drivers/Src/STM32F407xx_RCC_driver.c
@@ -8,7 +8,7 @@
 #include "STM32F407xx_RCC_driver.h"
 uint16_t AHB_PreScaler[8] = {2,4,8,16,64,128,256,512};
 uint8_t APB1_PreScaler[4] = { 2, 4 , 8, 16};
-
+uint8_t APB2_PreScaler[4] = { 2, 4 , 8, 16};
 uint32_t RCC_GetPCLK1Value(void){
 
 		uint32_t pclk1,SystemClk;
@@ -60,7 +60,50 @@ uint32_t RCC_GetPCLK1Value(void){
 
 
 uint32_t RCC_GetPCLK2Value(){
+	uint32_t pclk1,SystemClk;
 
+	uint8_t clksrc,temp,ahbp,apb2p;
+
+	clksrc = ((RCC->CFGR >> 2) & 0x3);
+
+	if(clksrc == 0 )
+	{
+		SystemClk = 16000000;
+	}else if(clksrc == 1)
+	{
+		SystemClk = 8000000;
+	}else if (clksrc == 2)
+	{
+//			SystemClk = RCC_GetPLLOutputClock();
+	}
+
+	//for ahb
+	temp = ((RCC->CFGR >> 4 ) & 0xF);
+
+	if(temp < 8)
+	{
+		ahbp = 1;
+	}else
+	{
+		ahbp = AHB_PreScaler[temp-8];
+	}
+
+
+
+	//apb2
+	temp = ((RCC->CFGR >> 13 ) & 0x7);
+
+	if(temp < 4)
+	{
+		apb2p = 1;
+	}else
+	{
+		apb2p = APB2_PreScaler[temp-4];
+	}
+
+	pclk1 =  (SystemClk / ahbp) /apb2p;
+
+	return pclk1;
 }
 
 
